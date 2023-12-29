61ee41c1bce311cb5005969b9d1038d22595d16b
Bkmz1941 <Bkmz1941@rambler.ru>
2023-12-29 12:28:51 +0100

    refact: README.md was added, Main class was refactored, Postman collection and mysql damp were added

--- src/main/java/ru/ilya/Main.java
+++ src/main/java/ru/ilya/Main.java
@@ -1,12 +1,7 @@
 package ru.ilya;
 
 import ru.ilya.core.DIContainer;
-import ru.ilya.database.Database;
 import ru.ilya.server.Server;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
